3914b4da92cae53f5f9eaaea9d580ba2f5c63dab
ZibiNandi01 <zibrinyinandi@gmail.com>
2026-03-06 17:27:19 +0100

    Auto fejlesztve lett, sokkal iranyithatobb

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -2,23 +2,35 @@ extends VehicleBody3D
 
 
 @export var MAX_STEER = 0.6
+var stearing_speed = 1.25
+var returning_speed = 2.5
 @export var ENGINE_POWER = 5000
 @export var BRAKE_POWER = 25
+var direction = 1
+@export var SPEEDOMETER_LABEL: Label
 
 func _physics_process(delta):
 	if steering>0:
 		if Input.get_axis("right", "left") > steering:
-			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *2)
+			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *stearing_speed)
 		else:
-			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *4)
+			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *returning_speed)
 	elif steering<0:
 		if Input.get_axis("right", "left") < steering:
-			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *2)
+			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *stearing_speed)
 		else:
-			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *4)
+			steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *returning_speed)
 	else:
-		steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *2)
+		steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta *stearing_speed)
 			
+	if Input.is_action_pressed("ahead"):
+		direction = 1
+		
+	if Input.is_action_pressed("reverse"):
+		direction = -1
 	
-	engine_force = int(Input.is_action_pressed("up")) * ENGINE_POWER
+	engine_force = int(Input.is_action_pressed("up")) * ENGINE_POWER * direction
 	brake = int(Input.is_action_pressed("down")) * BRAKE_POWER
+	
+	if SPEEDOMETER_LABEL:
+		SPEEDOMETER_LABEL.text = str(linear_velocity.length()*3.6)
