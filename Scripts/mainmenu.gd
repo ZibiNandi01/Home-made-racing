94f19cc97f08aa034494f5ab556b2d86e31e0a1f
ZibiNandi01 <zibrinyinandi@gmail.com>
2026-03-20 20:17:10 +0100

    A main menu user manual felület megcsinálva

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -1,19 +1,38 @@
 extends VBoxContainer
 
+@export var button_play: Button
+@export var button_user_manual:Button
+@export var label_user_manual: Label
+@export var button_back: Button
+	
 func _ready():
-	var button_play = Button.new()
+
 	button_play.text = "Play"
 	button_play.pressed.connect(_button_pressed_play)
-	add_child(button_play)
-	
-	var button_user_manual =Button.new()
+
 	button_user_manual.text = "User manual"
 	button_user_manual.pressed.connect(_button_pressed_user_manual)
-	add_child(button_user_manual)
+	
+	var text = FileAccess.open("res://User_manual.txt",FileAccess.READ)
+	label_user_manual.text = text.get_as_text()
+
+	button_back.text = "Back"
+	button_back.pressed.connect(_button_pressed_back)
+
 
 func _button_pressed_play():
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 
 func _button_pressed_user_manual():
-	pass
+	$Play.visible = false
+	$ButtonUserManual.visible = false
+	$LabelUserManual.visible = true
+	$Back.visible = true
+	
+func _button_pressed_back():
+	$LabelUserManual.visible = false
+	$Back.visible = false
+	$Play.visible = true
+	$ButtonUserManual.visible = true
+	
--- Scripts/user_manual.gd
+++ Scripts/user_manual.gd
@@ -1,13 +1,11 @@
 extends VBoxContainer
-
+#@export var label_user_manual = Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var label_user_manual = Label.new()
-	var text = FileAccess.open("res://User_manual.txt",FileAccess.READ)
-	label_user_manual.text = text.get_as_text()
-	add_child(label_user_manual)
-
+#	var text = FileAccess.open("res://User_manual.txt",FileAccess.READ)
+#	label_user_manual.text = text.get_as_text()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
